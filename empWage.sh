58787f4696749d68733ef36333b41fed49157e94
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-10 19:19:50 +0530

    Added : Employee Attendance

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,13 @@
-#Welcome to the employee wage computation program
+#!/bin/bash -x
 
+empCheck=$((RANDOM%2))
+isPresent=1
+
+case $empCheck in
+	$isPresent)
+		echo Employee is Present
+		;;
+	*)
+		echo Employee is Absent
+		;;
+esac
