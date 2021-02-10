fb6a523f1da42f7cc89d45ba7570bd2442128e04
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-10 19:46:21 +0530

    Added : Calculating Employee Daily Wage

--- empWage.sh
+++ empWage.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
+#constant
+EMP_RATE_PER_HR=20
+WORK_HR=8
+
+#variable
 empCheck=$((RANDOM%2))
 isPresent=1
 
 case $empCheck in
 	$isPresent)
-		echo Employee is Present
+		salary=$(($WORK_HR*$EMP_RATE_PER_HR))
+		echo $salary
 		;;
 	*)
 		echo Employee is Absent
